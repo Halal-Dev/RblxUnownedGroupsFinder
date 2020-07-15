49705bf2816103320c9be949ef8d6d4b75191fdf
HalalDev <StouffYT@gmail.com>
2020-07-15 20:34:37 +0000

    Hello WendoJ

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
-thread_count = input("number of threds ? (850 is best)")
+thread_count = int(input("number of threds ? (850 is best)"))
 import requests
 import json
 from random import randint
+import threading
+from lxml.html import fromstring
+from itertools import cycle
+import traceback
+import time
+from time import sleep
 
-def Check:
 
+
+with open("proxy.txt", "r") as fs:
+  proxies = [line.strip() for line in fs]
+proxy_pool = cycle(proxies)
+
+def Check():
+  for x in range(10000):
       headers = {
       'Accept': 'application/json',
       }
-      groupId = ""
-      for a in range(randint(1,6)):
-        groupId = groupId + str(randint(0,7))
-      groupId = groupId + str(randint(0,7))
+      groupId = str(randint(0,700000))
+        
       link = "https://groups.roblox.com/v1/groups/" + groupId
-      response = requests.get(link , headers=headers)
-      response = response.json()
-      print("testing " + groupId)
-      try:
-        if response['owner'] == None and response["publicEntryAllowed"] == True:
-            print(groupId + "cool")
-            with open("hit.txt", "a") as hit:
-                hit.write(groupId + " cool" + "\n")
-        else:
-            print("no " + groupId + " bad ")
+      proxy = next(proxy_pool)
+      try :
+          
+          response = requests.get(link , headers=headers, proxies={"http": proxy, "https": proxy})
+          response = response.json()
+          print("testing " + groupId)
+          try:
+              if response['owner'] == None and response["publicEntryAllowed"] == True:
+                print(groupId + "cool")
+                with open("hit.txt", "a") as hit:
+                    hit.write(groupId + " cool" + "\n")
+              else:
+                  print("no " + groupId + " bad ")
+                  with open("log.txt", "a") as hit:
+                      hit.write("no " + groupId + " bad ")
+              time.sleep(1)
+
+          except:
+            print("Owned")
             with open("log.txt", "a") as hit:
-                hit.write("no " + groupId + " bad ")
-
+                    hit.write(groupId + " Owned")
+            time.sleep(1)
       except:
-        print("Owned")
-        with open("log.txt", "a") as hit:
-                hit.write(groupId + " Owned")
-
+        print("proxy error")
 for x in range(thread_count):
     threading.Thread(target=Check).start()
 
